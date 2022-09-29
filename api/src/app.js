d72fc8254a70f0b2921abcd84c35c67eec292a90
Franco <juanfrancorodriguez99@gmail.com>
2022-09-29 14:22:23 -0300

    changed cors policy

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://pih-enry-client-twd1.vercel.app/"
+    "https://pih-enry-client-twd1.vercel.app"
   ); // update to match the domain you will make the request from, thank you this make me lost a lot of time
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
